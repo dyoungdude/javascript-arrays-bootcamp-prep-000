bfef6caafcba79f337a15ea7206c0e4cbad021a3
Darren Young <dlyoung5@asu.edu>
2018-07-17 03:20:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, '1'){
+  const array = ['two', 'three'];
+  ['1', ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, '1'){
+  var array = ['two', 'three'];
+  array.unshift('1');
+  return array;
+}
