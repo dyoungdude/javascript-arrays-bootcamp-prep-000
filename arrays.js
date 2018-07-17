82e46836046a9c32440692c0aff50f1af598999a
Darren Young <dlyoung5@asu.edu>
2018-07-17 03:24:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, '1'){
-  const array = ['two', 'three'];
-  ['1', ...array];
-  return array;
+function addElementToBeginningOfArray(chocolateBars, '1'){
+  ['1', ...chocolateBars];
+  return chocolateBars;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, '1'){
-  var array = ['two', 'three'];
-  array.unshift('1');
-  return array;
+function destructivelyAddElementToBeginningOfArray(chocolateBars, '1'){
+  chocolateBars.unshift('1');
+  return chocolateBars;
 }
